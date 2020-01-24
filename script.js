1f8604c15c028995a982453b82db0c73a74a1059
mohiganscs <56381261+mohiganscs@users.noreply.github.com>
2020-01-24 09:14:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,229 @@
-function toggleSidebar() {
-	document.getElementsByClassName('sidebar')[0].classList.toggle('active');
-	document.getElementsByClassName('toggle-btn')[0].classList.toggle('active');
+html, body {
+	margin: 0;
+	padding: 0;
+	font-family: sans-serif;
+	line-height: 2;
+	height: 100%;
 }
 
-function readMore(where) {
-	var dots = document.getElementById("dots-" + where)
-	var moreText = document.getElementById("more-" + where);
-	var btnText = document.getElementById("moreBtn-" + where);
+a {
+	text-decoration: none;
+}
 
-	if(dots.style.display == "none") {
-		dots.style.display = "inline";
-		btnText.innerHTML = "[Read More]";
-		moreText.style.display = "none";
-	} else {
-		dots.style.display = "none";
-		btnText.innerHTML = "[Read less]";
-		moreText.style.display = "inline";
+a:hover {
+	text-decoration: underline;
+}
+
+input[name="r"] {
+	position: absolute;
+	visibility: hidden;
+}
+
+.bottom {
+	height: 50px;
+}
+
+.container {
+	position: absolute;
+	top: 20%;
+	left: 50%;
+	transform: translateX(-50%);
+	display: block;
+	justify-content: center;
+	text-align: center;
+	width: 65%;
+	padding-bottom: 50px;
+}
+
+.logo {
+	color: white;
+}
+
+.link-arrow {
+	color: white;
+	font-size: 24px;
+	display: none;
+	cursor: pointer;
+}
+
+.link-list {
+	padding: 0;
+	display: flex;
+}
+
+.link-list li {
+	list-style-type: none;
+}
+
+.links {
+	text-transform: uppercase;
+	font-size: 14px;
+}
+
+.menu {
+	position: fixed;
+	top: 0;
+	width: 97.3%;
+	background: rgb(8, 8, 110);
+	padding: 30px 20px;
+	display: flex;
+	justify-content: space-between;
+	box-shadow: 0px 0px 20px 10px rgba(0, 0, 0, 0.6);
+	cursor: default;
+	z-index: 1;
+}
+
+.menu .menu-left {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	vertical-align: middle;
+}
+
+.menu .menu-left a {
+	color: rgba(255, 255, 255, 0.6);
+	text-decoration: none;
+	padding: 0 10px;
+}
+
+.menu .menu-left a:hover {
+	color: rgba(255, 255, 255, 0.9);
+	text-decoration: underline;
+}
+
+.menu .menu-left .logo {
+	font-weight: bold;
+	font-size: 24px;
+	padding: 0 20px 0 0;
+}
+
+/*---------------------------------Syllabus---------------------------------*/
+
+.syllabus {
+	position: relative;
+	left: 50%;
+	transform: translateX(-50%);
+	width: 100%;
+}
+
+.syllabus table {
+	width: 100%;
+	table-layout: fixed;
+	border-collapse: collapse;
+}
+
+.syllabus table tr {
+	border: 1px solid black;
+}
+
+.syllabus table tr td {
+	border: 1px solid black;
+}
+
+/*---------------------------------Slider---------------------------------*/
+
+.slide {
+	width: 20%;
+	transition: 0.6s;
+}
+
+.slide img {
+	width: 100%;
+	height: 100%;
+}
+
+.slider {
+	position: relative;
+	left: 50%;
+	transform: translateX(-50%);
+	width: 800px;
+	height: 400px;
+	overflow: hidden;
+	margin-top: 30px;
+}
+
+.slider-bar {
+	width: 50px;
+	height: 10px;
+	border: 2px solid #fff;
+	margin: 6px;
+	cursor: pointer;
+	transition: 0.4s;
+}
+
+.slider-bar:hover {
+	background: #fff;
+}
+
+.slider-nav {
+	position: absolute;
+	bottom: 20px;
+	left: 50%;
+	transform: translateX(-50%);
+	display: flex;
+}
+
+.slides {
+	width: 500%;
+	height: 100%;
+	display: flex;
+}
+
+#r1:checked ~ .s1 {
+	margin-left: 0;
+}
+
+#r2:checked ~ .s1 {
+	margin-left: -20%;
+}
+
+#r3:checked ~ .s1 {
+	margin-left: -40%;
+}
+
+#r4:checked ~ .s1 {
+	margin-left: -60%;
+}
+
+#r5:checked ~ .s1 {
+	margin-left: -80%;
+}
+
+/*---------------------------------Mobile---------------------------------*/
+
+@media only screen and (max-width: 768px) {
+
+	.container {
+		width: 90%;
+	}
+
+	.links .link-arrow {
+		display: block;
+	}
+
+	.links .link-list {
+		position: absolute;
+		top: 55px;
+		transform: translateX(-120px); 
+		display: none;
+		background: #e7e7e7;
+		width: 130px;
+	}
+
+	.links .link-list li a {
+		color: black;
+	}
+
+	.links .link-list li a:hover {
+		color: rgba(0, 0, 0, 0.6);
+	}
+
+	.menu {
+		justify-content: center;
+	}
+
+	.slider {
+		width: 105%;
+		height: auto;
 	}
 }
